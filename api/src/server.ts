314082d4406d87a384c1dee9a330d2e3dfd517e5
Madson Santana <madson.santana96@gmail.com>
2020-11-15 14:35:05 -0300

    config eslint

--- api/src/server.ts
+++ api/src/server.ts
@@ -3,7 +3,9 @@ import express from 'express'
 const app = express()
 
 app.get('/', (req, res) => {
- return res.json({ message: 'Ola Docker' })
+  return res.json({
+    message: 'Ola Docker', 
+  })
 })
 
 app.listen(3000, () => console.log('Rodando container'))
